23a6cb3e9a00b7df8f9f4ef3a861fe40a949d069
lswl66 <bgy359124261@outlook.com>
2019-10-09 20:40:54 +0800

    2.0

--- day1/remove-repeat.js
+++ day1/remove-repeat.js
@@ -1,14 +1,24 @@
+// function getNoRepeatArr (arr) {
+//     var insideArr = arr;
+//     var noRepeatArr = [];
+//     var i;
+//     var a;
+//     var l = insideArr.length;
+//     for (i = 0; i < l; i++) {
+//         a = insideArr.shift();
+//        if (insideArr.indexOf(a) == -1) {
+//            noRepeatArr.push(a);
+//        }
+//     }
+//     return noRepeatArr;
+// }
 function getNoRepeatArr (arr) {
     var insideArr = arr;
     var noRepeatArr = [];
-    var i;
-    var a;
-    var l = insideArr.length;
-    for (i = 0; i < l; i++) {
-        a = insideArr.shift();
-       if (insideArr.indexOf(a) == -1) {
-           noRepeatArr.push(a);
-       }
+    for (var i = 0; i < insideArr.length; i++) {
+        if (noRepeatArr.indexOf(insideArr[i]) === -1){
+            noRepeatArr.push(insideArr[i]);
+        }
     }
     return noRepeatArr;
 }
