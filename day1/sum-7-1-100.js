d61a59fbfb369d99a24d8d2b3ec541d202d36d87
lswl66 <bgy359124261@outlook.com>
2019-10-09 15:52:57 +0800

    lol

--- day1/sum-7-1-100.js
+++ day1/sum-7-1-100.js
@@ -1,7 +1,7 @@
 var i;
 var sum = 0;
-for (i=1; i<=100; i++) {
-    if(i%7===0){
+for (i = 1; i <= 100; i++) {
+    if(i % 7 === 0){
         sum += i;
     }
 }
--- day1/sum-even-1-100.js
+++ day1/sum-even-1-100.js
@@ -1,7 +1,7 @@
 var i;
 var sum = 0;
-for (i=1; i<=100; i++) {
-    if(i%2===0) {
+for (i = 1; i <=100; i++) {
+    if(i % 2 === 0) {
         sum += i;
     }
 }
